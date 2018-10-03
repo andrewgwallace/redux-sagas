0451628e74224c8a5b2eada1dda28e6daba74fd2
Andrew Wallace <awallace@1800flowers.com>
2018-10-03 18:35:49 -0400

    Add rootSaga and 1-second delay button

--- main.js
+++ main.js
@@ -3,11 +3,27 @@ import "babel-polyfill"
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga' // create the Saga middleware required to run the sagas
 
 import Counter from './Counter'
 import reducer from './reducers'
+// import { helloSaga } from './sagas' // import the helloSaga function you created destructured.
+import rootSaga from  './sagas' // import the rootSaga Generator function that runs all other Sagas. Notice that it is not destructured.
 
-const store = createStore(reducer)
+//Set the imported createSagaMiddleware into a variable
+const sagaMiddleware = createSagaMiddleware()
+
+// Introduce the saga middleware into the store
+// const store = createStore(reducer) // original
+const store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware)
+)
+// Run the imported sagas (in this case, just helloSaga)
+// sagaMiddleware.run(helloSaga)
+
+// Run the rootSaga which runs all the other Sagas
+sagaMiddleware.run(rootSaga)
 
 const action = type => store.dispatch({type})
 
@@ -16,7 +32,9 @@ function render() {
     <Counter
       value={store.getState()}
       onIncrement={() => action('INCREMENT')}
-      onDecrement={() => action('DECREMENT')} />,
+      onDecrement={() => action('DECREMENT')} 
+      // Add the connection for the onIncrementAsync call. Note that a plain object action is dispatched.
+      onIncrementAsync={() => action('INCREMENT_ASYNC')} />,
     document.getElementById('root')
   )
 }
---
+++ sagas.js
@@ -0,0 +1,39 @@
+// A collection of sagas that will run. In order to do so a Saga middleware needs to be created and connected to the Redux store.
+// The middleware is introduced into the main.js file.
+// the imported delay is a utility function that returns a Promise that will resolve after a specified number of milliseconds. 
+// This will be used to block the Generator.
+// Sagas are Generator functions that yield objets to the redux-saga middleware. Learn more here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/function*
+import { delay } from 'redux-saga' 
+import { put, takeEvery, all } from 'redux-saga/effects'
+
+export function* helloSaga() {
+    console.log('Hello Sagas!')
+}
+
+// Our worker Saga: will perform the async increment task. as it has the delay function, it will be suspended until the Promise is fulfilled.
+// Once resolved, the middleware resumes the Saga until the next yield. In this case, is then put. "put" is referred to as an Effect; A simple JS object which contains instruction to be fulfilled by the middleware.
+// The Saga is paused until the middleware fulfills the effect, in this case 'INCREMENT'
+export function* incrementAsync() {
+    yield delay(1000)
+    yield put({ type: 'INCREMENT' })
+}
+
+// Our watcher Saga: spawn a new incrementAsync task on each INCREMENT_ASYNC. 
+// takeEvery is a "helper" function by redux-saga that listens for dispatched actions.
+// In this case it's listening for the 'INCREMEMENT_ASYNC' action and when is hears it, 
+// it runs the passed argument, the incrementAsync Generator function above.
+export function* watchIncrementAsync() {
+    yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+}
+
+// Both sagas need to be started at once. To do this, a rootSaga will be created.
+// a rootSaga is responsible for starting out other Sagas. For this, we must import the "all" effect.
+// Note that "all" is passed an array of each Saga from within the file into an array being and being called "()".
+// Now that they've been exported and called to run at the same time, we just have to invoke sagaMiddleware.run on "rootSaga" in main.js.
+
+export default function* rootSaga() {
+    yield all([
+        helloSaga(),
+        watchIncrementAsync()
+    ])
+}
